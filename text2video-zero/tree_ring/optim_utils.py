b968ce49652bdac06590ad9591ecf320ec0c5c6e
Kuubrick <zengshunyang@stu.scu.edu.cn>
2024-05-04 00:52:52 +0800

    2024-05-04 0:52

--- text2video-zero/tree_ring/optim_utils.py
+++ text2video-zero/tree_ring/optim_utils.py
@@ -51,10 +51,10 @@ def image_distortion(img1, img2, seed, args):
         img2 = transforms.RandomRotation((args.r_degree, args.r_degree))(img2)
 
     if args.jpeg_ratio is not None:
-        img1.save(f"tmp_{args.jpeg_ratio}_{args.run_name}.jpg", quality=args.jpeg_ratio)
-        img1 = Image.open(f"tmp_{args.jpeg_ratio}_{args.run_name}.jpg")
-        img2.save(f"tmp_{args.jpeg_ratio}_{args.run_name}.jpg", quality=args.jpeg_ratio)
-        img2 = Image.open(f"tmp_{args.jpeg_ratio}_{args.run_name}.jpg")
+        img1.save(f"tmp_{args.jpeg_ratio}_img1.jpg", quality=args.jpeg_ratio)
+        img1 = Image.open(f"tmp_{args.jpeg_ratio}_img1.jpg")
+        img2.save(f"tmp_{args.jpeg_ratio}_img2.jpg", quality=args.jpeg_ratio)
+        img2 = Image.open(f"tmp_{args.jpeg_ratio}_img2.jpg")
 
     if args.crop_scale is not None and args.crop_ratio is not None:
         set_random_seed(seed)
